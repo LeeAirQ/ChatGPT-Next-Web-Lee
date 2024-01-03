866ff24869b88e85aef9c66e46154fed36fc579b
Leeyhenry <77561271+LeeAirQ@users.noreply.github.com>
2024-01-03 14:04:25 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { getServerSideConfig } from "./config/server";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Henry_Leey",
+  description: "Henry personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "NextChat",
+    title: "HenryChat",
     statusBarStyle: "default",
   },
 };
